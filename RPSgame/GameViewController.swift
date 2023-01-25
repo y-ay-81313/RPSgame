45f721cf34b609a557d2babdc7a1dd5f5a97e6de
y-ay-81313 <81313sky@gmail.com>
2023-01-25 17:20:24 +0900

    画面遷移実装(横スライド)

---
+++ RPSgame/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  RPSgame
+//
+//  Created by ay on 2023/01/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
--- RPSgame/ViewController.swift
+++ RPSgame/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let startButton = UIButton(frame: CGRect(x:40-10 , y: 150, width:300, height: 100))
+    let startButton = UIButton()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //navigationControllerの「戻る」ボタンのバーを消す
+        navigationController?.setNavigationBarHidden(true, animated: false)
         
         //ゲーム画面
         layout()
@@ -25,18 +28,36 @@ class ViewController: UIViewController {
     //レイアウト画面
     func layout() {
         
+        // スクリーンの横縦幅
+        let screenWidth:CGFloat = self.view.frame.width
+        let screenHeight:CGFloat = self.view.frame.height
+        
+        // ボタンの横縦幅
+        let startButtonWidth:CGFloat = 300
+        let startButtonHeight:CGFloat = 100
+        
+        //ゲーム開始ボタンの設定
+        startButton.frame = CGRect(x:screenWidth/2 - startButtonWidth/2,
+                                   y:screenHeight - startButtonHeight-80,
+                                   width:startButtonWidth,
+                                   height:startButtonHeight)
         startButton.setTitle("ゲーム開始", for: .normal)
         startButton.setTitleColor(UIColor.black, for: .normal)
         startButton.backgroundColor = UIColor.gray
         startButton.titleLabel?.font = UIFont.systemFont(ofSize: 30.0)
         
+        //ボタンが押されたときの処理
         startButton.addTarget(self, action: #selector(self.tapStartButton(_ :)), for: .touchUpInside)
         self.view.addSubview(startButton)
     }
     
     
     @objc func tapStartButton(_ sender: UIButton){
-    print("ボタンがタップされた")
+    
+        //ゲーム開始ボタンが押されたら、GameViewControllerに遷移する
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+        
     }
     
     
